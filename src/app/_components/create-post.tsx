52a2c6a463cff2801322d6569b893f422898c3b3
Francisco Cha <franciscocha@gmail.com>
2023-12-03 14:27:36 -1000

    Initial Commit

---
+++ src/app/_components/create-post.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { api } from "~/trpc/react";
+
+export function CreatePost() {
+	const router = useRouter();
+	const [name, setName] = useState("");
+
+	const createPost = api.post.create.useMutation({
+		onSuccess: () => {
+			router.refresh();
+			setName("");
+		},
+	});
+
+	return (
+		<form
+			onChange={(e) => {
+				e.preventDefault();
+				createPost.mutate({ name });
+			}}
+			className="flex flex-col gap-2"
+		>
+			<textarea
+				placeholder="Text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				className="textarea textarea-info textarea-bordered textarea-lg w-full"
+		/>
+		</form>
+	);
+}
---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+let post = {
+  name: "Hello World",
+};
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ input }) => {
+
+      var re = /(\b[a-z](?!\s))/g;
+      input.name = input.name.replace(re, function(x){return x.toUpperCase();});
+
+      post = { name: input.name };
+      return post;
+    }),
+
+  getLatest: publicProcedure.query(() => {
+    return post;
+  }),
+});
